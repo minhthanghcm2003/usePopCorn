6aaee01d22d9629a800c89823f209d70a0eff397
ngoctram168 <minhthanghcm2003@gmail.com>
2025-04-21 20:44:31 +0700

    First commit

--- src/Hooks/useMovie.tsx
+++ src/Hooks/useMovie.tsx
@@ -1,28 +1,13 @@
 import { useState, useEffect } from 'react';
-
 const KEY = '4f8058f2';
 
-interface Movie {
-  imdbID: string;
-  Title: string;
-  Year: string;
-  Poster: string;
-}
-
-export function useMovie(query: string) {
-  const [movies, setMovies] = useState<Movie[]>([]);
+export function useMovie(query: string, callback: () => void) {
+  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string>('');
-
+  const [error, setError] = useState('');
   useEffect(() => {
-    if (query.length < 3) {
-      setMovies([]);
-      setError('');
-      return;
-    }
-
+    callback?.();
     const controller = new AbortController();
-
     async function fetchMovie() {
       try {
         setIsLoading(true);
@@ -32,32 +17,31 @@ export function useMovie(query: string) {
           { signal: controller.signal }
         );
 
-        if (!res.ok) {
+        if (!res.ok)
           throw new Error('Something went wrong with fetching movies');
-        }
 
         const data = await res.json();
-        if (data.Response === 'False') {
-          throw new Error('Movie not found');
-        }
-
+        if (data.Response === 'False') throw new Error('Movie not found');
         setMovies(data.Search);
         setError('');
       } catch (err: any) {
         if (err.name !== 'AbortError') {
-          setError(err.message || 'An unknown error occurred');
+          setError(err.message);
         }
       } finally {
         setIsLoading(false);
       }
     }
-
+    if (query.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
+    // handleCloseMovie();
     fetchMovie();
-
-    return () => {
+    return function () {
       controller.abort();
     };
   }, [query]);
-
   return { movies, isLoading, error };
 }
